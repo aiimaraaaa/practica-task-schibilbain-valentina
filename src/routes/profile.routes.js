6f170f929f8c98a76b5fb8ff2b3af723cf8747d0
aimaraaaa <valen.schibilbain@gmail.com>
2026-08-23 17:40:23 -0300

    feat: se aplico las validaciones en las  rutas de Profile

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -3,8 +3,15 @@ import {
   createProfile,
   getProfiles,
 } from "../controllers/profile.controller.js";
+import { validate } from "../middlewares/validate.js";
+import { createProfileValidation } from "../middlewares/validations/profile.validation.js";
 
 export const profileRouter = Router();
 
-profileRouter.post("/profiles", createProfile);
+profileRouter.post(
+  "/profiles",
+  createProfileValidation,
+  validate,
+  createProfile,
+);
 profileRouter.get("/profiles", getProfiles);
