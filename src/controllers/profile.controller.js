04fc194711a89bcfc12470e4e5975f457dca556d
aimaraaaa <valen.schibilbain@gmail.com>
2026-08-23 17:36:44 -0300

    feat: se refactorizo profile.controller para usar matchedData

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,21 +1,15 @@
+import { matchedData } from "express-validator";
 import { ProfileModel } from "../models/profile.model.js";
 import { UserModel } from "../models/user.model.js";
 
 export const createProfile = async (req, res) => {
   try {
-    const { bio, avatar, userId } = req.body;
-
-    if (!userId) {
-      return res.status(400).json({ message: "El userId es obligatorio" });
-    }
-
-    const user = await UserModel.findByPk(userId);
-    if (!user) {
-      return res.status(404).json({ message: "Usuario no encontrado" });
-    }
-
-    const profile = await ProfileModel.create({ bio, avatar, userId });
-    return res.status(201).json({ message: "Perfil creado", profile });
+    const validatedData = matchedData(req);
+    const profile = await ProfileModel.create(validatedData);
+    return res.status(201).json({
+      message: "Perfil creado exitosamente",
+      profile,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Error interno del servidor" });
