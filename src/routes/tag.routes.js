f25f67f71bbecccdd02c269cd9922ed29d35f652
aimaraaaa <valen.schibilbain@gmail.com>
2026-08-23 17:41:04 -0300

    feat: se aplico las validaciones en las rutas de Tag

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { createTag, getTags } from "../controllers/tag.controller.js";
+import { validate } from "../middlewares/validate.js";
+import { createTagValidation } from "../middlewares/validations/tag.validation.js";
 
 export const tagRouter = Router();
 
-tagRouter.post("/tags", createTag);
+tagRouter.post("/tags", createTagValidation, validate, createTag);
 tagRouter.get("/tags", getTags);
