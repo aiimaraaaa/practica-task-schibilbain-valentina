0918329b5e6ddcec073527008f3eecf114e433bb
aimaraaaa <valen.schibilbain@gmail.com>
2026-08-23 17:39:28 -0300

    feat: se aplico validaciones en rutas de Task

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import {
-  crearTask,
+  createTask,
+  deleteTask,
   getAllTasks,
   getTaskById,
   updateTask,
-  deleteTask,
 } from "../controllers/task.controller.js";
+import { validate } from "../middlewares/validate.js";
+import {
+  createTaskValidation,
+  updateTaskValidation,
+} from "../middlewares/validations/task.validation.js";
 
 export const taskRouter = Router();
 
-taskRouter.post("/tasks", crearTask);
+taskRouter.post("/tasks", createTaskValidation, validate, createTask);
 taskRouter.get("/tasks", getAllTasks);
 taskRouter.get("/tasks/:id", getTaskById);
-taskRouter.put("/tasks/:id", updateTask);
+taskRouter.put("/tasks/:id", updateTaskValidation, validate, updateTask);
 taskRouter.delete("/tasks/:id", deleteTask);
