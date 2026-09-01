a83ac155d9a6cbd90ac1d2fc2568db89cb850f95
aimaraaaa <valen.schibilbain@gmail.com>
2026-09-01 18:43:03 -0300

    feat: se agrego el updateTag y deleteTag

--- src/controllers/tag.controller.js
+++ src/controllers/tag.controller.js
@@ -33,3 +33,45 @@ export const getTags = async (req, res) => {
     return res.status(500).json({ message: "Error interno del servidor" });
   }
 };
+
+export const updateTag = async (req, res) => {
+  try {
+    const validatedDataBody = matchedData(req, { locations: ["body"] });
+    const { id } = matchedData(req, { locations: ["params"] });
+
+    const tagExist = await TagModel.findByPk(id);
+
+    if (!tagExist) {
+      return res.status(404).json({ message: "Etiqueta no encontrada" });
+    }
+
+    await tagExist.update(validatedDataBody);
+
+    return res.status(200).json({
+      message: "Etiqueta actualizada exitosamente",
+      tag: tagExist,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+export const deleteTag = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const tagExist = await TagModel.findByPk(id);
+
+    if (!tagExist) {
+      return res.status(404).json({ message: "Etiqueta no encontrada" });
+    }
+
+    await tagExist.destroy();
+
+    return res.status(200).json({ message: "Etiqueta eliminada exitosamente" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
