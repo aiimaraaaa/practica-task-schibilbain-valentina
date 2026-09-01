4487c700edf91ee05127462c249cbc18009933db
aimaraaaa <valen.schibilbain@gmail.com>
2026-09-01 18:45:28 -0300

    feat: se le agrego las rutas PUT y DELETE para Tag

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { createTag, getTags } from "../controllers/tag.controller.js";
+import {
+  createTag,
+  getTags,
+  updateTag,
+  deleteTag,
+} from "../controllers/tag.controller.js";
 import { validate } from "../middlewares/validate.js";
-import { createTagValidation } from "../middlewares/validations/tag.validation.js";
+import {
+  createTagValidation,
+  updateTagValidation,
+} from "../middlewares/validations/tag.validation.js";
 
 export const tagRouter = Router();
 
 tagRouter.post("/tags", createTagValidation, validate, createTag);
 tagRouter.get("/tags", getTags);
+tagRouter.put("/tags/:id", updateTagValidation, validate, updateTag);
+tagRouter.delete("/tags/:id", deleteTag);
