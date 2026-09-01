b878e16b0db73829b3fc8aa80e67df9ab8b7206b
aimaraaaa <valen.schibilbain@gmail.com>
2026-09-01 18:38:33 -0300

    feat: se agrego la validacion de params y .optional() en Tag

--- src/middlewares/validations/tag.validation.js
+++ src/middlewares/validations/tag.validation.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { TagModel } from "../../models/tag.model.js";
 
 export const createTagValidation = [
@@ -16,14 +16,28 @@ export const createTagValidation = [
     }),
 ];
 
-export const idTagValidation = [
-  body("id")
+export const updateTagValidation = [
+  param("id")
     .isInt({ min: 1 })
     .withMessage("El ID debe ser un número entero positivo")
     .custom(async (id) => {
       const tag = await TagModel.findByPk(id);
       if (!tag) {
         throw new Error("Etiqueta no encontrada");
+      }
+      return true;
+    }),
+
+  body("name")
+    .optional()
+    .notEmpty()
+    .withMessage("El nombre no puede estar vacío")
+    .isLength({ max: 50 })
+    .withMessage("El nombre no puede superar los 50 caracteres")
+    .custom(async (name, { req }) => {
+      const tag = await TagModel.findOne({ where: { name } });
+      if (tag && tag.id !== parseInt(req.params.id)) {
+        throw new Error("Ya existe otra etiqueta con ese nombre");
       }
       return true;
     }),
