090181022edbc32a3fd5683ffd1959ed2b9ac1e4
aimaraaaa <valen.schibilbain@gmail.com>
2026-09-01 18:42:08 -0300

    feat: le agregue el  updateProfile y deleteProfile con paranoid

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -33,3 +33,45 @@ export const getProfiles = async (req, res) => {
     return res.status(500).json({ message: "Error interno del servidor" });
   }
 };
+
+export const updateProfile = async (req, res) => {
+  try {
+    const validatedDataBody = matchedData(req, { locations: ["body"] });
+    const { id } = matchedData(req, { locations: ["params"] });
+
+    const profileExist = await ProfileModel.findByPk(id);
+
+    if (!profileExist) {
+      return res.status(404).json({ message: "Perfil no encontrado" });
+    }
+
+    await profileExist.update(validatedDataBody);
+
+    return res.status(200).json({
+      message: "Perfil actualizado exitosamente",
+      profile: profileExist,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+export const deleteProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const profileExist = await ProfileModel.findByPk(id);
+
+    if (!profileExist) {
+      return res.status(404).json({ message: "Perfil no encontrado" });
+    }
+
+    await profileExist.destroy();
+
+    return res.status(200).json({ message: "Perfil eliminado exitosamente" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
